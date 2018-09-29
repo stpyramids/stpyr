af447ac2decad61bc3ff65b1ed78b7d430c74213
Matthew Boeh <m@mboeh.com>
2018-09-28 22:00:43 -0700

    Separate out lib from main

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+extern crate specs;
+#[macro_use]
+extern crate specs_derive;
+extern crate line_drawing;
+extern crate ncurses;
+extern crate pathfinding;
+
+pub mod action;
+pub mod ai;
+pub mod behavior;
+pub mod curses;
+pub mod energy;
+pub mod events;
+pub mod fov;
+pub mod grid;
+pub mod log;
+pub mod map;
+pub mod movement;
+pub mod player;
+pub mod pos;
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
 extern crate specs;
 #[macro_use]
 extern crate specs_derive;
-extern crate line_drawing;
-extern crate ncurses;
-extern crate pathfinding;
+extern crate stpyrl;
 
-pub mod action;
-pub mod ai;
-pub mod behavior;
-pub mod curses;
-pub mod energy;
-pub mod events;
-pub mod fov;
-pub mod grid;
-pub mod log;
-pub mod map;
-pub mod movement;
-pub mod player;
-pub mod pos;
-
-use self::pos::*;
 use specs::prelude::*;
+use stpyrl::{pos::*, *};
 
 fn main() {
     let default_panic = std::panic::take_hook();
